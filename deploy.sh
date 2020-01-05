3dca032bbf06e4dee3fd1d59df2c16a5bc707b04
aanper <mail@s3f.ru>
2020-01-05 17:33:50 +0300

    mqtt send ok, trouble in modubs-controller side

---
+++ deploy.sh
@@ -0,0 +1,1 @@
+scp package.json index.js backspace.csv pi@$HOST:/opt/backspace_acs_controller
--- index.js
+++ index.js
@@ -3,9 +3,9 @@ let mqtt = require("mqtt");
 let client  = mqtt.connect('mqtt://192.168.0.100', {username:"modbus-gateway", password:"ipR9Lsj8sPZvj2rpzLEtY"});
 
 const topics = [
-    "modbus/rpi/state/door1_opened",
-    "modbus/rpi/state/door2_opened",
-    "modbus/rpi/state/door3_opened",
+    "modbus/switchboard_controller_input/state/door1_opened",
+    "modbus/switchboard_controller_input/state/door2_opened",
+    "modbus/switchboard_controller_input/state/door3_opened",
 ];
 
 client.on('connect', () => {
@@ -21,13 +21,13 @@ client.on('connect', () => {
 client.on('message', (topic, message) => {
     // message is Buffer
     console.log(topic, message.toString());
-    if(topic === "modbus/rpi/state/door3_opened") {
+    if(topic === "modbus/switchboard_controller_input/state/door3_opened") {
         if(message.toString() === "True") {
             console.log("open portal, sw on light");
-            client.publish('modbus/rpi/set/portal_light', new Uint8Array([1]));
+            client.publish('modbus/switchboard_controller_relay/set/portal_light', "True");
         } else {
             console.log("closed portal, sw off light");
-            client.publish('modbus/rpi/set/portal_light', new Uint8Array([0]));
+            client.publish('modbus/switchboard_controller_relay/set/portal_light', "False");
         }
     }
     // client.end()
